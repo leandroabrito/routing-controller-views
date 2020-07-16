5b74653856999f269a6efdcf1f345cf34626f7eb
leandro brito <leandroabrito89@gmail.com>
2020-07-16 20:26:11 -0300

    add links and create custom path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get '/about', to: "pages#about"
+  # get '/about', to: "pages#about", as: :about_us
+
 
   get '/contact', to: "pages#contact"
 
